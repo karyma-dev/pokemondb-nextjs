56c905dc7b5818da2f449dfd10abb44d2ec0afee
Kary Ma <karyma101@hotmail.com>
2020-10-24 21:31:17 -0700

    added typescript rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
             jsx: true // Allows for the parsing of JSX
         }
     },
-    rules: {},
+    rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off'
+    },
     settings: {
         react: {
             version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
