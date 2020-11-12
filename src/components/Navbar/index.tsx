98f118439361765a887d41e08b9a733cce113f67
Kary Ma <karyma101@hotmail.com>
2020-11-12 13:15:52 -0800

    add links to list elements

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Autocomplete from '../Autocomplete'
 import styles from './index.module.css'
 
@@ -6,9 +7,18 @@ const Navbar = () => {
     return (
         <ul className={styles.ul}>
             <div className={`container ${styles.container}`}>
-                <li className={styles.li}>Pokedex</li>
-                <li className={styles.li}>Berries</li>
-                <Autocomplete />
+                <li className={styles.li}>
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/pokedex">Pokedex</Link>
+                </li>
+                <li className={styles.li}>
+                    <Link href="/berry">Berries</Link>
+                </li>
+                <li className={styles.li}>
+                    <Autocomplete />
+                </li>
             </div>
         </ul>
     )
