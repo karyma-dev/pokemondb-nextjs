bcc56ab28d9458aa069aa21bf78980f8d9c10d74
Kary Ma <karyma101@hotmail.com>
2020-11-13 17:22:50 -0800

    Add SVG types for typescript

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.svg?inline' {
+    const content: any
+    export default content
+}
+  
+declare module '*.svg' {
+    const content: any
+    export default content
+}
